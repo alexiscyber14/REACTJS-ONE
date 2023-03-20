62ffac0f83828a135256ea6c27c489c8c8861fb8
alexiscyber14 <142sledgehammer14@gmail.com>
2023-03-20 19:16:18 +0100

    Add functions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
 import React from 'react';
-import { CalculatorComponent } from './Components/calculator';
+import CalculatorComponent from './Components/calculator';
 
-class App extends React.Component {
-  styles={
-
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <CalculatorComponent />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <CalculatorComponent />
+  </div>
+);
 
 export default App;
--- src/Components/calculator.js
+++ src/Components/calculator.js
@@ -1,40 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../App.css';
 
-export class CalculatorComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const CalculatorComponent = () => (
+  <div className="parent">
+    <div className="child output dark-gray">0</div>
+    <button className="child light-gray" type="button">AC</button>
+    <button className="child light-gray" type="button">+/-</button>
+    <button className="child light-gray" type="button">%</button>
+    <button className="child orange" type="button">÷</button>
+    <button className="child light-gray" type="button">7</button>
+    <button className="child light-gray" type="button">8</button>
+    <button className="child light-gray" type="button">9</button>
+    <button className="child orange" type="button">x</button>
+    <button className="child light-gray" type="button">4</button>
+    <button className="child light-gray" type="button">5</button>
+    <button className="child light-gray" type="button">6</button>
+    <button className="child orange" type="button">-</button>
+    <button className="child light-gray" type="button">1</button>
+    <button className="child light-gray" type="button">2</button>
+    <button className="child light-gray" type="button">3</button>
+    <button className="child orange" type="button">+</button>
+    <button className="child zero light-gray" type="button">0</button>
+    <button className="child light-gray" type="button">.</button>
+    <button className="child orange" type="button">=</button>
 
-    };
-  }
-
-  render() {
-    return (
-      <div className="parent">
-        <div className="child output dark-gray">0</div>
-        <button className="child light-gray" type="button">AC</button>
-        <button className="child light-gray" type="button">+/-</button>
-        <button className="child light-gray" type="button">%</button>
-        <button className="child orange" type="button">÷</button>
-        <button className="child light-gray" type="button">7</button>
-        <button className="child light-gray" type="button">8</button>
-        <button className="child light-gray" type="button">9</button>
-        <button className="child orange" type="button">x</button>
-        <button className="child light-gray" type="button">4</button>
-        <button className="child light-gray" type="button">5</button>
-        <button className="child light-gray" type="button">6</button>
-        <button className="child orange" type="button">-</button>
-        <button className="child light-gray" type="button">1</button>
-        <button className="child light-gray" type="button">2</button>
-        <button className="child light-gray" type="button">3</button>
-        <button className="child orange" type="button">+</button>
-        <button className="child zero light-gray" type="button">0</button>
-        <button className="child light-gray" type="button">.</button>
-        <button className="child orange" type="button">=</button>
-
-      </div>
-    );
-  }
-}
+  </div>
+);
 export default CalculatorComponent;
